3cb5936ef958395765f0fbfef827602fee5b1e22
Nicolas Rouault <trouba@gmail.com>
2016-05-17 11:15:45 +0200

    add space model, faker gem and seed for space

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+# Creating 100 fake restaurants
+10.times do
+  space = Space.new({
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    daily_price: Faker::Number.between(10, 20),
+    weekly_price: Faker::Number.between(40, 90),
+    description: Faker::Hipster.paragraph,
+    has_food: Faker::Boolean.boolean,
+    has_drink: Faker::Boolean.boolean,
+    has_wifi: Faker::Boolean.boolean,
+    phone_number: Faker::PhoneNumber.phone_number,
+    max_people: Faker::Number.between(5, 50),
+    lat: Faker::Address.latitude,
+    lng: Faker::Address.longitude
+  })
+  space.save
+end
