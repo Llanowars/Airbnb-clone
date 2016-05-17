3a1e5e81b9b459371527fe5f2d01c59901682bff
Nicolas Rouault <trouba@gmail.com>
2016-05-17 16:23:08 +0200

    add space creation form without validation

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,9 +1,17 @@
 class SpacesController < ApplicationController
   def new
+    @space = Space.new
   end
 
 
   def create
+    @space = Space.create(space_params)
+    @space.save
+    if @space.save
+      redirect_to space_path(@space)
+    else
+      render "new"
+    end
   end
 
   def update
@@ -22,4 +30,14 @@ class SpacesController < ApplicationController
   def show
     @space = Space.find(params[:id])
   end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :address, :daily_price, :weekly_price, :has_wifi, :has_drinks, :has_food, :description, :max_people )
+  end
+
 end
+
+
+
