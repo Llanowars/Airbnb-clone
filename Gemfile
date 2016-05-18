a6c3a3292e70a906e75313a2a21b06643b7bd0ec
Nicolas Rouault <trouba@gmail.com>
2016-05-18 16:05:46 +0200

    add geocoder and spaces index layout

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
+source 'https://rails-assets.org' do
+  gem "rails-assets-underscore"
+end
 ruby '2.3.0'
 
 gem 'rails', '4.2.6'
@@ -21,6 +24,8 @@ gem 'cloudinary'
 gem "attachinary"
 gem "jquery-fileupload-rails"
 gem "coffee-rails"
+gem "gmaps4rails"
+gem "geocoder"
 
 group :development, :test do
   gem 'binding_of_caller'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517162755) do
+ActiveRecord::Schema.define(version: 20160518122109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20160517162755) do
     t.float    "lng"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "spaces", ["user_id"], name: "index_spaces_on_user_id", using: :btree
